3650b86b850f9d4255174383caeea7ef83aed086
Kecheng Bi <bikecheng@gmail.com>
2019-03-26 14:05:19 +0800

    Abstract log part into utils

--- utils/utils.go
+++ utils/utils.go
@@ -5,23 +5,32 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"os"
+	"path/filepath"
+
+	log "github.com/sirupsen/logrus"
 )
 
+//Logger global log object
+var Logger *log.Logger
+
+//PrettyPrint Print json to console elegantly, which is helpful for degbugging
 func PrettyPrint(data interface{}, pre string, post string) {
-	dataJson, err := json.MarshalIndent(data, "", "\t")
+	dataJSON, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		fmt.Printf("Data cannot be reformatted, the original data is:\n %#v\n", data)
 	} else {
 		if pre != "" {
 			fmt.Printf("%s\n", pre)
 		}
-		fmt.Printf("%s\n", dataJson)
+		fmt.Printf("%s\n", dataJSON)
 		if post != "" {
 			fmt.Printf("%s\n", post)
 		}
 	}
 }
 
+//GetLoginOptions Get login information
 func GetLoginOptions() (string, string, string, error) {
 	var server, username, password string
 	flag.StringVar(&server, "server", "", "Server address")
@@ -31,8 +40,69 @@ func GetLoginOptions() (string, string, string, error) {
 
 	if server == "" || username == "" || password == "" {
 		flag.PrintDefaults()
-		return "", "", "", errors.New("server, username, and password must all be specified.")
+		return "", "", "", errors.New("server, username, and password must all be specified")
+	}
+	return server, username, password, nil
+}
+
+//GetLogLevel Get the log level based on input string
+//Valid string: panic fatal error warning info debug trace
+func GetLogLevel(logStr string) log.Level {
+	level, err := log.ParseLevel(logStr)
+	if err != nil {
+		return log.DebugLevel
+	}
+	return level
+}
+
+//InitLogger Init global log object
+func InitLogger(filename string, levelStr string) {
+	var defaultLogFile = "storagemetric.log"
+	var level log.Level
+
+	logger := log.New()
+
+	if filename == "" {
+		filename = filepath.Join(os.TempDir(), defaultLogFile)
+	}
+	f, err := os.OpenFile(filename, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644)
+	if err != nil {
+		logger.SetOutput(os.Stdout)
 	} else {
-		return server, username, password, nil
+		logger.SetOutput(f)
+	}
+
+	envLogStr, ok := os.LookupEnv("STORAGEMETRIC_LOGLEVEL")
+	if ok {
+		level = GetLogLevel(envLogStr)
+	} else {
+		level = GetLogLevel(levelStr)
+	}
+	logger.SetLevel(level)
+
+	if level == log.DebugLevel {
+		logger.SetReportCaller(true)
+	}
+	Logger = logger
+}
+
+//Log log message based on log level
+func Log(levelStr string, message string) {
+	level := GetLogLevel(levelStr)
+	switch level {
+	case log.PanicLevel:
+		Logger.Panic(message)
+	case log.FatalLevel:
+		Logger.Fatal(message)
+	case log.ErrorLevel:
+		Logger.Error(message)
+	case log.WarnLevel:
+		Logger.Warn(message)
+	case log.InfoLevel:
+		Logger.Info(message)
+	case log.DebugLevel:
+		Logger.Debug(message)
+	case log.TraceLevel:
+		Logger.Trace(message)
 	}
 }
