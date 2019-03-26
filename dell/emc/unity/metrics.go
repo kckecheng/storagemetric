97159727d77e35d2aca4a8ab4e1888234547e806
Kecheng Bi <bikecheng@gmail.com>
2019-03-26 14:37:50 +0800

    Leverage utils for log

--- dell/emc/unity/metrics.go
+++ dell/emc/unity/metrics.go
@@ -2,8 +2,11 @@ package unity
 
 import (
 	"fmt"
+
+	"github.com/kckecheng/storagemetric/utils"
 )
 
+// NewMetricRealTimeQuery Create a real time query
 func (unity *Unity) NewMetricRealTimeQuery(paths []string, interval int) (int, error) {
 	payload := struct {
 		Paths    []string `json:"paths"`
@@ -13,44 +16,46 @@ func (unity *Unity) NewMetricRealTimeQuery(paths []string, interval int) (int, e
 	err := unity.Request("POST", "/api/types/metricRealTimeQuery/instances", "", "", payload, &ret)
 
 	if err != nil {
-		unity.Log("error", "Fail to create a new metric real time query")
+		utils.Log("error", "Fail to create a new metric real time query")
 		return 0, err
-	} else {
-		id := ret.Content.Id
-		unity.Log("debug", fmt.Sprintf("New Metric Real Time Query ID: %d", id))
-		return id, nil
 	}
+	id := ret.Content.Id
+	utils.Log("debug", fmt.Sprintf("New Metric Real Time Query ID: %d", id))
+	return id, nil
 }
 
+// MetricRealTimeQueryExisted Check if a real time query with the specified ID exists
 func (unity *Unity) MetricRealTimeQueryExisted(id int) bool {
-	unity.Log("debug", fmt.Sprintf("Check if the specified query id %d exists", id))
+	utils.Log("debug", fmt.Sprintf("Check if the specified query id %d exists", id))
 	err := unity.Request("GET", fmt.Sprintf("/api/instances/metricRealTimeQuery/%d", id), "", "", "", nil)
 	if err != nil {
-		unity.Log("warning", fmt.Sprintf("The query id %d does not exist", id))
+		utils.Log("warning", fmt.Sprintf("The query id %d does not exist", id))
 		return false
-	} else {
-		unity.Log("info", fmt.Sprintf("The query id %d exists", id))
-		return true
 	}
+	utils.Log("info", fmt.Sprintf("The query id %d exists", id))
+	return true
 }
 
+// DeleteMetricRealTimeQuery Delete a real time query based on its ID
 func (unity *Unity) DeleteMetricRealTimeQuery(id int) error {
-	unity.Log("debug", fmt.Sprintf("Delete metric real time query %d", id))
+	utils.Log("debug", fmt.Sprintf("Delete metric real time query %d", id))
 	uri := fmt.Sprintf("/api/instances/metricRealTimeQuery/%d", id)
 	err := unity.Request("DELETE", uri, "", "", nil, nil)
 	return err
 }
 
+// GetMetricQueryResult Get metric results
 // Pitfall: Metric will be empty if it is retrivded without waiting for at least a query interval after creating the query
 func (unity *Unity) GetMetricQueryResult(id int, result *Metric) error {
-	unity.Log("debug", fmt.Sprintf("Get metric result with id %d", id))
+	utils.Log("debug", fmt.Sprintf("Get metric result with id %d", id))
 	filter := fmt.Sprintf("queryId eq %d", id)
 	err := unity.Request("GET", "/api/types/metricQueryResult/instances", "", filter, nil, result)
 	return err
 }
 
+// GetHistoricalMetric Query historial metrics
 func (unity *Unity) GetHistoricalMetric(path string, result *Metric) error {
-	unity.Log("debug", fmt.Sprintf("Get historical metric data with path %s", path))
+	utils.Log("debug", fmt.Sprintf("Get historical metric data with path %s", path))
 	filter := fmt.Sprintf("path eq \"%s\"", path)
 	err := unity.Request("GET", "/api/types/metricValue/instances", "", filter, nil, result)
 	return err
--- dell/emc/unity/unity.go
+++ dell/emc/unity/unity.go
@@ -6,76 +6,38 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	log "github.com/sirupsen/logrus"
 	"io/ioutil"
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httputil"
-	"os"
-	"path/filepath"
+
+	"github.com/kckecheng/storagemetric/utils"
 )
 
+// Unity Unity array object
 type Unity struct {
 	server   string
 	username string
 	password string
 	token    string
 	client   http.Client
-	logger   *log.Logger
 }
 
-func getLogLevel(logStr string) log.Level {
-	//Valid string: panic fatal error warning info debug trace
-	level, err := log.ParseLevel(logStr)
-	if err != nil {
-		return log.DebugLevel
-	} else {
-		return level
-	}
-}
-
-func initLogger(filename string, levelStr string) *log.Logger {
-	var defaultLogFile string = "storagemetric.log"
-	var level log.Level
-
-	logger := log.New()
-
-	if filename == "" {
-		filename = filepath.Join(os.TempDir(), defaultLogFile)
-	}
-	f, err := os.OpenFile(filename, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644)
-	if err != nil {
-		logger.SetOutput(os.Stdout)
-	} else {
-		logger.SetOutput(f)
-	}
-
-	envLogStr, ok := os.LookupEnv("STORAGEMETRIC_LOGLEVEL")
-	if ok {
-		level = getLogLevel(envLogStr)
-	} else {
-		level = getLogLevel(levelStr)
-	}
-	logger.SetLevel(level)
-
-	if level == log.DebugLevel {
-		logger.SetReportCaller(true)
-	}
-
-	return logger
-}
-
+// URL join server base address with a URI and return the final full URL
 func URL(server, URI string) string {
 	return "https://" + server + URI
 }
 
+// New Init Unity Object
 func New(server string, username string, password string) (*Unity, error) {
-	logger := initLogger("", "")
+	if utils.Logger == nil {
+		utils.InitLogger("", "")
+	}
 
 	var err error
-	logger.Debug(fmt.Sprintf("server: %s, username: %s, password: %s", server, username, password))
+	utils.Log("debug", fmt.Sprintf("server: %s, username: %s, password: %s", server, username, password))
 	if server == "" || username == "" || password == "" {
-		return nil, errors.New("Unity server address, username, and password must be specified.")
+		return nil, errors.New("Unity server address, username, and password must be specified")
 	}
 
 	cookieJar, _ := cookiejar.New(nil)
@@ -89,19 +51,19 @@ func New(server string, username string, password string) (*Unity, error) {
 	req.Header.Set("X-EMC-REST-CLIENT", "true")
 
 	reqDetails, _ := httputil.DumpRequest(req, true)
-	logger.Debug(fmt.Sprintf("Login Request: %s", string(reqDetails)))
+	utils.Log("debug", fmt.Sprintf("Login Request: %s", string(reqDetails)))
 	resp, err := client.Do(req)
 	if err != nil {
-		logger.Error(err.Error())
+		utils.Log("error", err.Error())
 		return nil, err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
 		message := fmt.Sprintf("Login %s with %s/%s failed.", server, username, password)
-		logger.Error(message)
+		utils.Log("error", message)
 		respDetails, _ := httputil.DumpResponse(resp, true)
-		logger.Debug(fmt.Sprintf("Login Response: %s", string(respDetails)))
+		utils.Log("debug", fmt.Sprintf("Login Response: %s", string(respDetails)))
 		return nil, errors.New(message)
 	}
 
@@ -113,23 +75,23 @@ func New(server string, username string, password string) (*Unity, error) {
 		password: password,
 		token:    token,
 		client:   client,
-		logger:   logger,
 	}, nil
 }
 
+// Request Send get/post/delete request
 func (unity *Unity) Request(method string, URI string, fields string, filter string, payload interface{}, result interface{}) error {
 	requestParams := fmt.Sprintf("method: %s, URI: %s, fields: %s, filter: %s, payload: %#v", method, URI, fields, filter, payload)
-	unity.Log("debug", requestParams)
+	utils.Log("debug", requestParams)
 	if method == "" || URI == "" {
-		return errors.New("method, or URI is missed.")
+		return errors.New("method, or URI is missed")
 	}
 
 	url := "https://" + unity.server + URI
 
 	var req *http.Request
 	if payload != nil {
-		payloadJson, _ := json.Marshal(payload)
-		req, _ = http.NewRequest(method, url, bytes.NewBuffer(payloadJson))
+		payloadJSON, _ := json.Marshal(payload)
+		req, _ = http.NewRequest(method, url, bytes.NewBuffer(payloadJSON))
 	} else {
 		req, _ = http.NewRequest(method, url, nil)
 	}
@@ -154,17 +116,17 @@ func (unity *Unity) Request(method string, URI string, fields string, filter str
 	req.URL.RawQuery = params.Encode()
 
 	reqDetails, _ := httputil.DumpRequest(req, true)
-	unity.Log("debug", fmt.Sprintf("Request: %s", string(reqDetails)))
+	utils.Log("debug", fmt.Sprintf("Request: %s", string(reqDetails)))
 
 	resp, err := unity.client.Do(req)
 	if err != nil {
-		unity.Log("error", err.Error())
+		utils.Log("error", err.Error())
 		return err
 	}
 	defer resp.Body.Close()
 
 	respDetails, _ := httputil.DumpResponse(resp, true)
-	unity.Log("debug", string(respDetails))
+	utils.Log("debug", string(respDetails))
 	if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
 		respRaw, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
@@ -176,34 +138,14 @@ func (unity *Unity) Request(method string, URI string, fields string, filter str
 		}
 
 		return nil
-	} else {
-		unity.Log("error", "Fail to perform the request")
-		return fmt.Errorf("Request Fails: %s", requestParams)
 	}
+	utils.Log("error", "Fail to perform the request")
+	return fmt.Errorf("Request Fails: %s", requestParams)
 }
 
-func (unity *Unity) Log(levelStr string, message string) {
-	level := getLogLevel(levelStr)
-	switch level {
-	case log.PanicLevel:
-		unity.logger.Panic(message)
-	case log.FatalLevel:
-		unity.logger.Fatal(message)
-	case log.ErrorLevel:
-		unity.logger.Error(message)
-	case log.WarnLevel:
-		unity.logger.Warn(message)
-	case log.InfoLevel:
-		unity.logger.Info(message)
-	case log.DebugLevel:
-		unity.logger.Debug(message)
-	case log.TraceLevel:
-		unity.logger.Trace(message)
-	}
-}
-
+// Destroy logout Unity
 func (unity *Unity) Destroy() error {
-	unity.Log("debug", "Logout")
+	utils.Log("debug", "Logout")
 	err := unity.Request("POST", "/api/types/loginSessionInfo/action/logout", "", "", nil, nil)
 	return err
 }
